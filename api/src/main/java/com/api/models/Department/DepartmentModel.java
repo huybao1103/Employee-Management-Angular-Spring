bc4a4e9824ee4ab9cdddd0002a384964315f766f
huybao1103 <l.q.h.bao@gmail.com>
2026-01-27 17:12:56 +0700

    split basic department attribute

--- api/src/main/java/com/api/models/Department/DepartmentModel.java
+++ api/src/main/java/com/api/models/Department/DepartmentModel.java
@@ -6,11 +6,7 @@ import com.api.models.Employee.EmployeeModel;
 
 import java.util.List;
 
-public class DepartmentModel extends BaseModel {
-    public String id;
-    public String name;
-    public String address;
-    public String email;
+public class DepartmentModel extends DepartmentBasicInfoModel {
     public Employee department_head;
     public List<EmployeeModel> employees;
 }
--- api/src/main/java/com/api/models/Employee/EmployeeBasicModel.java
+++ api/src/main/java/com/api/models/Employee/EmployeeBasicModel.java
@@ -1,11 +1,24 @@
 package com.api.models.Employee;
 
 import com.api.models.BaseModel;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class EmployeeBasicModel extends BaseModel {
     public String id;
+
+    @NotNull
+    @Size(min = 1, max = 200)
     public String name;
+
+    @Email
     public String email;
+
+    @NotNull
+    @DecimalMin("0")
     public double salary;
+
     public String createdAt;
 }
