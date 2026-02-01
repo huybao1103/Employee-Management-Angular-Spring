96722fd6da688d1fcf1faa605bef2c44152fb0ab
huybao1103 <l.q.h.bao@gmail.com>
2026-02-01 20:56:34 +0700

    fix lombok issue

--- api/src/main/java/com/api/util/Exception/ApiResult.java
+++ api/src/main/java/com/api/util/Exception/ApiResult.java
@@ -1,12 +1,5 @@
 package com.api.util.Exception;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class ApiResult {
-    private int statusCode;
-    private String message;
+public record ApiResult(int statusCode, String message) {
 }
